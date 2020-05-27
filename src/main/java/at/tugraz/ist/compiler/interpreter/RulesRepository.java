86fd090f111bd7dbf6aa8af0ede1f87dc00b5efe
Martin Zimmermann <martin.zimmermann.git@outlook.com>
2020-05-27 20:59:38 +0200

    Added hierarchical search

--- src/main/java/at/tugraz/ist/compiler/interpreter/RulesRepository.java
+++ src/main/java/at/tugraz/ist/compiler/interpreter/RulesRepository.java
@@ -1,31 +1,83 @@
 package at.tugraz.ist.compiler.interpreter;
 
-import at.tugraz.ist.compiler.rule.InterpreterRule;
 import at.tugraz.ist.compiler.rule.Predicate;
 import at.tugraz.ist.compiler.rule.Rule;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class RulesRepository {
 
+    class Node {
+        HashMap<String, Node> ruleNodes = new HashMap<>();
+        List<Rule> rules = new ArrayList<>();
+
+        public Node getNode(String name) {
+            return ruleNodes.get(name);
+        }
+
+        public void addRule(Rule rule) {
+            rules.add(rule);
+        }
+
+        public Node getOrCreateNode(String name) {
+            if(!ruleNodes.containsKey(name))
+                ruleNodes.put(name, new Node());
+
+            return ruleNodes.get(name);
+        }
+
+        public boolean hasRules() {
+            return !rules.isEmpty();
+        }
+
+        public boolean containsNode(String name) {
+            return ruleNodes.containsKey(name);
+        }
+
+        public List<Rule> getRules() {
+            return new ArrayList<>(rules);
+        }
+
+        public void removeRule(Rule rule) {
+            rules.remove(rule);
+        }
+    }
+
     private List<Rule> rules;
+    Node root;;
 
     public RulesRepository(List<Rule> rules) {
+        this.root = new Node();
         this.rules = rules;
 
-
-
+        for(Rule rule : rules) {
+            Node current = root;
+            for(Predicate pred : rule.getPreconditions()) {
+                current = current.getOrCreateNode(pred.getName());
+            }
+            current.addRule(rule);
+        }
     }
 
     public void add(Rule rule) {
-        rules.add(rule);
+        this.rules.add(rule);
+
+        Node current = root;
+        for(Predicate pred : rule.getPreconditions()) {
+            current = current.getOrCreateNode(pred.getName());
+        }
+        current.addRule(rule);
     }
 
     public void remove(Rule rule) {
-        rules.remove(rule);
+        this.rules.remove(rule);
+
+        Node current = root;
+        for(Predicate pred : rule.getPreconditions()) {
+            current = current.getOrCreateNode(pred.getName());
+        }
+        current.removeRule(rule);
+        //TODO: implement pruning
     }
 
     public List<Rule> getRules() {
@@ -33,8 +85,25 @@ public class RulesRepository {
     }
 
     public List<Rule> getPossibleRules(Set<Predicate> current_memory) {
-        return rules.stream()
-                .filter(rule -> current_memory.containsAll(rule.getPreconditions()))
-                .collect(Collectors.toList());
+        List<Node> horizond = new ArrayList<>();
+        horizond.add(root);
+
+
+        for(Predicate pred: current_memory ) {
+            List<Node> toAdd = new ArrayList<>();
+            for(Node node : horizond) {
+                if(node.containsNode(pred.getName()))
+                    toAdd.add(node.getNode(pred.getName()));
+            }
+            horizond.addAll(toAdd);
+        }
+
+        List<Rule> rules = new ArrayList<>();
+        for(Node node : horizond) {
+            if(node.hasRules()){
+                rules.addAll(node.getRules());
+            }
+        }
+        return rules;
     }
 }
